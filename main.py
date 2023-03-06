af54d9c19e1be874b016521752fd3b3da454d6ed
AhmedKamal75 <rtya89@gmail.com>
2023-03-06 15:41:13 +0200

    second commit

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pca import pca
 # from sklearn.model_selection import train_test_split
 
 
-
 # counting from 0
 def split_dataset_oscillate(X, y):
     X_train = []
@@ -14,7 +13,7 @@ def split_dataset_oscillate(X, y):
     y_train = []
     y_test = []
     for i in range(X.shape[0]):
-        if (i%2) == 0: # even
+        if (i % 2) == 0:  # even
             X_test.append(X[i])
             y_test.append(y[i])
         else:
@@ -24,10 +23,9 @@ def split_dataset_oscillate(X, y):
     return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
 
 
-
 if __name__ == "__main__":
     X, y = reformat_dataset(path=os.path.join("archive"))
     # X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.5)
-    X_train, X_test, y_train, y_test = split_dataset_oscillate(X,y)
-    print(pca(X_train,built_in=False))
-    print(pca(X_test,built_in=False))
+    X_train, X_test, y_train, y_test = split_dataset_oscillate(X, y)
+    print(pca(X_train, built_in=False))
+    # print(pca(X_test,built_in=False))
--- pca.py
+++ pca.py
@@ -3,17 +3,24 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 
 
-def pca(dataset,a=alphanums,built_in=True,normalize=True):
+def pca(dataset, alpha=0.95, built_in=True, normalize=False):
     if normalize:
-        normalized_data = MinMaxScaler().fit_transform(dataset)
+        dataset = MinMaxScaler().fit_transform(dataset)
     if built_in:
-         print(alpha)
-         pca = PCA(n_components=alpha)
-         X_pca = pca.fit_transform(normalized_data)
+        pca = PCA(n_components=alpha)
+        X_pca = pca.fit_transform(dataset)
     else:
-        data_mean = 
-    return X_pca
+        # mean for every feature
+        mean_vector = np.array([np.mean(dataset[:, i]) for i in range(dataset.shape[1])])
+        # centered data
+        Z = dataset - mean_vector
+        # covariance matrix S, it is symmetric matrix
+        S = (np.matmul(Z.transpose(), Z)) / Z.shape[1]
+        # get eigenvalues w, eigenvectors v ordered increasingly
+        w, v = np.linalg.eigh(S)
 
+    # return X_pca
+    return "this is the return value"
 
 
 if __name__ == "__main__":
